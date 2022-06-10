bce97a282f56ef6c581574a6a05463d8e28cde8c
EJ Gonzalez <e.gonzalez@itrustcapital.com>
2022-06-09 23:32:39 -0700

    adding tailwind

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="decoration-red decoration-dotted">EJ Gonzalez</header>
+  );
+};
+
+export default Header;
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,22 +1,43 @@
-import React from 'react'
+import React from "react";
 
-export default function Project(props){
-    return (
-        <li>
-            <h2>{props.title}</h2>
-            <p><span className='bold'>Front-End: </span>{props.front}</p>
-            {props.back == "" 
-                ? 
-                <p></p>
-                :
-                <p><span className='bold'>Back-End: </span>{props.back}</p>
-            }
-            <p>{props.details}</p>
-            <img src={props.img} alt={props.title}/>
-            <div className='btn_div'>
-                <a className='btn' target='_blank' rel="noopener noreferrer" href={props.live}>Live</a>
-                {props.repo == "" ? null : <a className='btn' target='_blank' rel="noopener noreferrer" href={props.repo}>Repo</a>}
-            </div>
-        </li>
-    )
+export default function Project(props) {
+  return (
+    <li>
+      <h2>{props.title}</h2>
+      <p>
+        <span className="bol">Front-End: </span>
+        {props.front}
+      </p>
+      {props.back ? (
+        <p></p>
+      ) : (
+        <p>
+          <span className="bold">Back-End: </span>
+          {props.back}
+        </p>
+      )}
+      <p>{props.details}</p>
+      <img src={props.img} alt={props.title} />
+      <div className="btn_div">
+        <a
+          className="btn"
+          target="_blank"
+          rel="noopener noreferrer"
+          href={props.live}
+        >
+          Live
+        </a>
+        {props.repo ? null : (
+          <a
+            className="btn"
+            target="_blank"
+            rel="noopener noreferrer"
+            href={props.repo}
+          >
+            Repo
+          </a>
+        )}
+      </div>
+    </li>
+  );
 }
